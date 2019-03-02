ab13c6928c796bca64c28ba03013a2c448e7a5d1
John Zimmerman <john.zimmerman@simplybusiness.com>
2019-03-02 03:37:35 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,9 @@
 age_of_person = 19
 
+year = 2003
+
 def current_age_for_birth_year(year)
   puts Time.now.year - current_age_for_birth_year
 end
 
-current_age_for_birth_year(2003)
+current_age_for_birth_year(year)
